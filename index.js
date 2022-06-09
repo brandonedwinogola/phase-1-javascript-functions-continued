9e68238a0cf3aa68d92ce2ed501297f2970b4dc4
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-09 20:20:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun() {
+    console.log("This saturay,I want to code!");
+};
+
+const fn = function(mondayWork) {
+    console.log("This Monday,I Will go to the office");
+};
+
+function saturdayFun() {
+    console.log("code")
+}
+
+function mondayWork() {
+    console.log('go to office')
+}
+
+function outer("wrapAdjective") {
+    const innerFuction = function("brandon") {
+        return $ { "You are brandon" }
+        const encouragingPromptFunction = wrapAdjective("!!!")
+
+        wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+    }
+
+}
